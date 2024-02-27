46193ee348cbf36bc3be5f429b310c023297e24b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2024-02-27 23:20:55 +0100

    4.058 - Repàs SQL 16 (exercicis), complet

--- 0405-DDL-DML/04_058-exercicis-repasSQL16.sql
+++ 0405-DDL-DML/04_058-exercicis-repasSQL16.sql
@@ -66,27 +66,176 @@ INSERT INTO esDe VALUES ('4.058', '1', 'CT');
 
 -- 5. Mostra els noms de conceptes, ordenats alfabèticament.
 
+SELECT nom
+FROM conceptes 
+ORDER BY nom;
+
+
 -- 6. Mostra la quantitat d'apartats que té cada exercici.
+
+SELECT nom, COUNT(*) AS Quantitat_Apartats
+FROM exercicis JOIN apartats 
+ON exercicis.codi = apartats.codi_exercici 
+GROUP BY nom;
+
 
 -- 7. Afig un nou concepte, detallant tots els camps en l'orde INSERT: EX - EXIST / NOT EXIST
 
-INSERT INTO conceptes (codi, nom) VALUES ('4.058', 'Actors');
+INSERT INTO conceptes(codi, nom) VALUES ('EX', 'EXIST / NOT EXIST');
 
 
 -- 8. Has de corregir el concepte "EX": ha de ser "EXISTS / NOT EXISTS".
 
+UPDATE conceptes 
+SET nom = 'EXISTS / NOT EXISTS'
+WHERE codi = 'EX';
+
+
 -- 9, 10, 11, 12. Mostra el nom de l'exercici que més apartats tinga, de 4 formes diferents.
+
+-- 9a LIMIT (SQLite)
+
+SELECT nom
+FROM exercicis JOIN apartats 
+ON exercicis.codi = apartats.codi_exercici 
+GROUP BY nom
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+-- 9b FETCH (Oracle)
+
+SELECT nom
+FROM exercicis JOIN apartats 
+ON exercicis.codi = apartats.codi_exercici 
+GROUP BY nom
+ORDER BY COUNT(*) DESC
+FETCH NEXT 1 ROWS ONLY;
+
+-- 10 COUNT = MAX
+
+SELECT nom 
+FROM exercicis JOIN apartats 
+ON exercicis.codi = apartats.codi_exercici 
+GROUP BY nom 
+HAVING COUNT(*) =
+(
+    SELECT MAX(quantitat)  FROM (
+        SELECT COUNT(*) AS quantitat
+        FROM exercicis JOIN apartats 
+        ON exercicis.codi = apartats.codi_exercici 
+        GROUP BY nom 
+    )
+);
+
+-- 11 ALL (No en SQLite)ç
+
+SELECT nom
+FROM exercicis JOIN apartats 
+ON exercicis.codi = apartats.codi_exercici 
+GROUP BY nom 
+HAVING COUNT(*) >= ALL
+(
+    SELECT COUNT(*)
+    FROM exercicis JOIN apartats 
+    ON exercicis.codi = apartats.codi_exercici 
+    GROUP BY nom 
+);
+
+-- 12 Not Exists
+
+SELECT e1.nom
+FROM exercicis e1 JOIN apartats a1
+ON e1.codi = a1.codi_exercici 
+GROUP BY e1.nom 
+HAVING NOT EXISTS
+(
+    SELECT e2.nom
+    FROM exercicis e2 JOIN apartats a2
+    ON e2.codi = a2.codi_exercici 
+    GROUP BY e2.nom 
+    HAVING COUNT(a2.num) > COUNT(a1.num)
+);
+
 
 -- 13. Modifica la taula "apartats" per a afegir un camp "descripcio".
 
+-- Oracle
+
+ALTER TABLE apartats ADD (descripcio VARCHAR2(50));
+
+-- SQLite
+
+ALTER TABLE apartats ADD COLUMN descripcio VARCHAR2(50);
+
+
 -- 14. Mostra els apartats per als quals encara no tenim descripció.
+
+SELECT descripcio 
+FROM apartats 
+WHERE descripcio IS NULL;
+
 
 -- 15, 16. Mostra els apartats el número dels quals estiga entre 1 i 5 i pertanguen a l'exercici 4.058, de 2 formes diferents
 
+SELECT num 
+FROM apartats 
+WHERE num BETWEEN 1 AND 5 AND codi_exercici = '4.058';
+
+SELECT num 
+FROM apartats 
+WHERE num >= 1 AND num <= 5 AND codi_exercici = '4.058';
+
+
 -- 17. Esborra el concepte "F1", si existix.
+
+DELETE FROM conceptes WHERE codi = 'F1';
+
 
 -- 18. Mostra el codi i nom de l'exercici, juntament amb el nom de l'apartat, per als repassos del concepte CREATE TABLE.
 
+SELECT e.codi, a.num, e.nom 
+FROM exercicis e JOIN apartats a
+ON e.codi = a.codi_exercici 
+JOIN esde 
+ON a.codi_exercici = esde.codi_exercici AND a.num = esde.num_apartat
+JOIN conceptes c
+ON esde.codi_concepte = c.codi 
+WHERE UPPER(c.nom) = 'CREATE TABLE';
+
+
 -- 19, 20, 21. Mostra els conceptes que comencen per "CREA" (potser amb majúscules diferents), de 3 formes diferents.
 
+--Substring
+
+SELECT *
+FROM conceptes 
+WHERE UPPER(SUBSTR(nom,1,4)) = 'CREA';
+
+--Instr
+
+SELECT *
+FROM conceptes 
+WHERE INSTR(UPPER(nom),'CREA') = 1;
+
+--Like
+
+SELECT *
+FROM conceptes 
+WHERE UPPER(nom) LIKE 'CREA%';
+
+
 -- 22. Mostra nom i codi dels dos últims exercicis (segons orde alfabètic)
+
+-- Oracle
+
+SELECT nom, codi
+FROM exercicis 
+ORDER BY nom DESC
+FETCH NEXT 2 ROWS ONLY;
+
+-- SQLite
+
+SELECT nom, codi
+FROM exercicis 
+ORDER BY nom DESC
+LIMIT 2;
