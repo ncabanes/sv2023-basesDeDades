92dee3bb35c996831410a4e6ee63a1e45be339de
Nacho Cabanes <ncabanes@users.noreply.github.com>
2024-03-26 18:32:57 +0100

    6.010 - ELSIF

---
+++ 06-PLSQL/06_010a-elsif.sql
@@ -0,0 +1,36 @@
+/*
+Crea una versió millorada de l'exercici anterior: a partir de les dades 
+de la taula de jocs, crea un script que guarde en una variable la 
+quantitat de jocs que tenim de la plataforma el codi de la qual és 
+"ps3". En una altra variable, guarda la quantitat de jocs que tenim de 
+la plataforma el codi de la qual és "ps4". Finalment, mostra el 
+missatge "Tenim més jocs de PS3 que de PS4", o bé "Tenim més jocs de 
+PS4 que de PS3", o "Tenim la mateixa quantitat de jocs de PS3 que de 
+PS4", segons corresponga.
+
+Versió 1: ELSIF (l'alternativa raonable)
+*/
+
+DECLARE
+    jocsPS3 jocs.codiPlataforma%type;
+    jocsPS4 jocs.codiPlataforma%type;
+BEGIN
+    SELECT COUNT(*) 
+        INTO jocsPS3
+        FROM jocs
+        WHERE codiPlataforma= 'PS3';
+
+    SELECT COUNT(*) 
+        INTO jocsPS4
+        FROM jocs
+        WHERE codiPlataforma= 'PS4';
+
+    IF jocsPS3>jocsPS4 THEN
+        dbms_output.put_line('Tenim mes jocs de ps3 que de ps4');
+    ELSIF jocsPS3<jocsPS4 THEN
+        dbms_output.put_line('Tenim mes jocs de ps4 que de ps3');
+    ELSE
+        dbms_output.put_line('Tenim la mateixa quantitat de ps3 que de ps4');
+    END IF;
+END;
+    
