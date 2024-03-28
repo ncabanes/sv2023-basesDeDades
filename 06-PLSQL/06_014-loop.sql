56ade98d9c44034db77e1f4352a691e0bb34d299
Nacho Cabanes <ncabanes@users.noreply.github.com>
2024-03-28 19:00:44 +0100

    6.014 - LOOP

---
+++ 06-PLSQL/06_014-loop.sql
@@ -0,0 +1,14 @@
+/*
+Crea un bucle LOOP que mostre els números del 10 al 100, augmentant de 
+10 en 10. Has d'emprar la sintaxi EXIT WHEN.
+*/
+
+DECLARE
+    i NUMBER(4) := 10;
+BEGIN
+    LOOP
+        dbms_output.put_line(i);
+        EXIT WHEN i >= 100;
+        i := i + 10;
+    END LOOP;
+END;
