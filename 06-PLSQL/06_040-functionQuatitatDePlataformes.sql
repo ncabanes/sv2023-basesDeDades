2978c5f31bf9844cb8a4d6b0cc38b21251e210c1
Nacho Cabanes <ncabanes@users.noreply.github.com>
2024-04-26 11:11:08 +0200

    6.040 - FUNCTION QuantitatDePlataformes

---
+++ 06-PLSQL/06_040-functionQuatitatDePlataformes.sql
@@ -0,0 +1,33 @@
+-- Crea una funció "QuantitatDePlataformes", que retorne el text "cap", 
+-- "una", més d'una", segons quantes plataformes hi haja en la nostra base 
+-- de dades. Has d'emprar COUNT.
+
+
+
+CREATE OR REPLACE FUNCTION QuantitatDePlataformes
+RETURN VARCHAR AS
+
+    quantitat NUMBER(4);
+
+BEGIN
+    SELECT COUNT(*)
+    INTO quantitat
+    FROM plataformes;
+    
+    IF quantitat = 0 THEN
+        RETURN 'cap';
+    ELSIF quantitat = 1 THEN
+        RETURN 'una';
+    ELSE
+        RETURN 'més d''una';
+    END IF;
+
+END QuantitatDePlataformes;
+
+BEGIN
+    dbms_output.put_line('Plataformes: '|| QuantitatDePlataformes);
+END;
+
+-- Alternativa
+
+SELECT QuantitatDePlataformes FROM dual;
